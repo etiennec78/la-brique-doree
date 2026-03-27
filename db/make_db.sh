e68f0f41f135343b66addfb0d619f7afb7770e05
Etienne C. <59794011+etiennec78@users.noreply.github.com>
2026-03-27 15:49:00 +0100

    Convert database from sqlite to MariaDB

--- db/make_db.sh
+++ db/make_db.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
-DB_NAME="brique_doree.db"
+DB_NAME="brique_doree"
 SQL_FILE="schema.sql"
 
-if [ -f "$DB_NAME" ]; then
+sudo systemctl start mariadb
+
+mariadb --version
+
+if [ $? -ne 0 ]; then
+    echo "Something went wrong ! Do you have mariadb installed on your system ?"
+    exit 2
+fi
+
+DB_EXISTS=$(sudo mariadb -u root -e "SHOW DATABASES LIKE '$DB_NAME';" | grep "$DB_NAME")
+
+if [ ! -z "$DB_EXISTS" ]; then
     echo "A database already exists. Are you sure you want to replace it by an empty one ?"
     echo "Type 'DELETE' to continue"
     read confirmation
     if [[ "$confirmation" == "DELETE" ]]; then
         echo "Deleting the previous database..."
-        rm "$DB_NAME"
+        sudo mariadb -e "DROP DATABASE $DB_NAME;"
     else
         echo "The database was not deleted. Exiting..."
         exit 1
@@ -17,15 +28,24 @@ if [ -f "$DB_NAME" ]; then
 fi
 
 echo "Making the database from $SQL_FILE..."
-sqlite3 "$DB_NAME" < "$SQL_FILE"
+sudo mariadb -e "CREATE DATABASE $DB_NAME;"
 
 if [ $? -ne 0 ]; then
-    echo "Something went wrong ! Do you have sqlite3 installed on your system ?"
-    exit 2
+    echo "Error: Could not create database $DB_NAME !"
+    exit 4
 fi
 
-if [ -f "$DB_NAME" ]; then
+sudo mariadb "$DB_NAME" < "$SQL_FILE"
+
+if [ $? -ne 0 ]; then
+    echo "Error: Could not import $SQL_FILE into the $DB_NAME database !"
+    exit 5
+fi
+
+if [ $? -eq 0 ]; then
+    echo "----------------------------------------------"
     echo "Success ! The database '$DB_NAME' was created."
+    echo "The MariaDB service is running."
 else
     echo "The database could not be created !"
     exit 3
--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,8 @@
 ----- ALLERGENES -----
 
 CREATE TABLE allergen (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT UNIQUE NOT NULL
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(25) UNIQUE NOT NULL
 );
 
 INSERT INTO allergen (name) VALUES
@@ -12,16 +12,16 @@ INSERT INTO allergen (name) VALUES
 ----- NUTRISCORE et TIME_SLOT -----
 
 CREATE TABLE nutriscore (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name char(1) UNIQUE NOT NULL
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name CHAR(1) UNIQUE NOT NULL
 );
 
 INSERT INTO nutriscore (name) VALUES
 ('A'), ('B'), ('C'), ('D'), ('E');
 
 CREATE TABLE time_slot (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT UNIQUE NOT NULL
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(7) UNIQUE NOT NULL
 );
 
 INSERT INTO time_slot (name) VALUES
@@ -31,19 +31,19 @@ INSERT INTO time_slot (name) VALUES
 ----- FOOD -----
 
 CREATE TABLE food (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name varchar(30) NOT NULL,
-    price float NOT NULL,
-    description varchar(255) NOT NULL,
-    image_path varchar(50) NOT NULL,
-    nutriscore_id INTEGER,
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    price FLOAT NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    image_path VARCHAR(50) NOT NULL,
+    nutriscore_id INT,
     FOREIGN KEY(nutriscore_id) REFERENCES nutriscore(id)
 );
 
 -- Table de liaison pour les allergènes
 CREATE TABLE food_allergen (
-    food_id INTEGER,
-    allergen_id INTEGER,
+    food_id INT,
+    allergen_id INT,
     PRIMARY KEY (food_id, allergen_id),
     FOREIGN KEY (food_id) REFERENCES food(id) ON DELETE CASCADE,
     FOREIGN KEY (allergen_id) REFERENCES allergen(id) ON DELETE CASCADE
@@ -53,18 +53,18 @@ CREATE TABLE food_allergen (
 ----- MENU -----
 
 CREATE TABLE menu (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name varchar(30) NOT NULL,
-    price float NOT NULL,
-    min_people INTEGER,
-    time_slot_id INTEGER,
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    price FLOAT NOT NULL,
+    min_people INT,
+    time_slot_id INT,
     FOREIGN KEY(time_slot_id) REFERENCES time_slot(id)
 );
 
 -- Table de liaison entre Menu et Food
 CREATE TABLE menu_food (
-    menu_id INTEGER,
-    food_id INTEGER,
+    menu_id INT,
+    food_id INT,
     PRIMARY KEY (menu_id, food_id),
     FOREIGN KEY (menu_id) REFERENCES menu(id) ON DELETE CASCADE,
     FOREIGN KEY (food_id) REFERENCES food(id) ON DELETE CASCADE
@@ -74,28 +74,28 @@ CREATE TABLE menu_food (
 ----- ROLES et USERS -----
 
 CREATE TABLE role (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT UNIQUE NOT NULL
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(15) UNIQUE NOT NULL
 );
 
 INSERT INTO role (name) VALUES
 ('client'), ('restaurateur'), ('administrator'), ('delivery_person');
 
 CREATE TABLE users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    email varchar(255) NOT NULL,
-    password_hash varchar(255) NOT NULL,
-    name varchar(255),
-    role_id INTEGER NOT NULL,
-    phone varchar(20),
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    name VARCHAR(255),
+    role_id INT NOT NULL,
+    phone VARCHAR(20),
     birth_date DATE,
     inscription_date DATETIME,
     last_connection DATETIME,
-    street_nb INTEGER,
-    street_nb_ext varchar(3),
-    street TEXT,
-    town TEXT,
-    zip_code INTEGER,
+    street_nb INT,
+    street_nb_ext VARCHAR(3),
+    street VARCHAR(50),
+    town VARCHAR(50),
+    zip_code INT,
     FOREIGN KEY(role_id) REFERENCES role(id)
 );
 
@@ -103,10 +103,10 @@ CREATE TABLE users (
 ----- COUPONS -----
 
 CREATE TABLE coupon (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    code varchar(20) UNIQUE NOT NULL,
-    discount_percent float,
-    is_active BOOLEAN DEFAULT 1,
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    code VARCHAR(20) UNIQUE NOT NULL,
+    discount_percent FLOAT,
+    is_active TINYINT(1) DEFAULT 1,
     expiration_date DATETIME
 );
 
@@ -115,8 +115,8 @@ CREATE TABLE coupon (
 
 
 CREATE TABLE payment_status (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name varchar(20) UNIQUE NOT NULL
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(20) UNIQUE NOT NULL
 );
 
 INSERT INTO payment_status (name) VALUES
@@ -125,12 +125,11 @@ INSERT INTO payment_status (name) VALUES
 
 -- Le panier
 CREATE TABLE cart (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    user_id INTEGER NOT NULL,
-    delivery_address TEXT,
-    payment_status_id INTEGER,
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    payment_status_id INT,
     created_at DATETIME NOT NULL,
-    coupon_id INTEGER,
+    coupon_id INT,
     FOREIGN KEY(payment_status_id) REFERENCES payment_status(id),
     FOREIGN KEY(user_id) REFERENCES users(id),
     FOREIGN KEY(coupon_id) REFERENCES coupon(id)
@@ -138,9 +137,9 @@ CREATE TABLE cart (
 
 -- Table de liaison entre les menus et le panier
 CREATE TABLE cart_menu (
-    cart_id INTEGER,
-    menu_id INTEGER,
-    quantity INTEGER DEFAULT 1,
+    cart_id INT,
+    menu_id INT,
+    quantity INT DEFAULT 1,
     PRIMARY KEY (cart_id, menu_id),
     FOREIGN KEY (cart_id) REFERENCES cart(id) ON DELETE CASCADE,
     FOREIGN KEY (menu_id) REFERENCES menu(id) ON DELETE CASCADE
@@ -150,12 +149,12 @@ CREATE TABLE cart_menu (
 ----- PAIEMENT -----
 
 CREATE TABLE payment (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    cart_id INTEGER NOT NULL,
-    user_id INTEGER NOT NULL,
-    bank_details_token varchar(255) NOT NULL,
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    cart_id INT NOT NULL,
+    user_id INT NOT NULL,
+    bank_details_token VARCHAR(255) NOT NULL,
     transaction_date DATETIME NOT NULL,
-    amount float NOT NULL,
+    amount FLOAT NOT NULL,
     FOREIGN KEY(cart_id) REFERENCES cart(id),
     FOREIGN KEY(user_id) REFERENCES users(id)
 );
