4bf14525dabb15d704b8b5b40f6f182edd41a6d6
MartinJdy <jeudy.martin14@gmail.com>
2026-04-29 15:30:22 +0200

    Bouton pour changer de charte

--- includes/header.php
+++ includes/header.php
@@ -52,5 +52,41 @@
             <?php else: ?>
                 <a href="/login" class="navbarbutton">Connexion</a>
             <?php endif; ?>
+
+            <button id="theme-toggle" class="navbarbutton">🌙</button>
         </section>
     </header>
+
+<script>
+    let btn = document.getElementById("theme-toggle");
+    let body = document.body;
+
+    function majIcone() {
+        if (body.classList.contains("light-theme")) {
+            btn.innerText = "🌙";
+        } else {
+            btn.innerText = "☀️";
+        }
+    }
+
+    window.addEventListener("DOMContentLoaded", () => {
+        if (document.cookie.includes("theme=light")) {
+            body.classList.add("light-theme");
+        }
+        majIcone(); 
+    });
+
+    btn.addEventListener("click", () => {
+        body.classList.toggle("light-theme");
+        majIcone(); 
+
+        let theme;
+        if (body.classList.contains("light-theme")) {
+            theme = "light";
+        } else {
+            theme = "black";
+        }
+
+        document.cookie = "theme=" + theme + "; max-age=2592000; path=/";
+    });
+</script>
