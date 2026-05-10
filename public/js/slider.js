3c12f525211e84e0080ce11da2ff061f13e74151
MartinJdy <jeudy.martin14@gmail.com>
2026-05-10 16:11:03 +0200

    Fix Slides

--- public/js/slider.js
+++ public/js/slider.js
@@ -2,10 +2,14 @@ const slideshow = document.getElementById('slideshow');
 const slides = document.querySelectorAll('.slide');
 let index = 0;
 
-document.getElementById('next').onclick = () => {
+function slideSuivant() {
     index = (index + 1) % slides.length;
-    slideshow.style.transform = `translateX(-${index * 25}%)`;
-};
+    slideshow.style.transform = `translateX(-${index * 25}%)`; /
+}
+
+setInterval(slideSuivant, 5000); //répète la fonction toutes les 5 secondes
+
+document.getElementById('next').onclick = slideSuivant;
 
 document.getElementById('prev').onclick = () => {
     index = (index - 1 + slides.length) % slides.length;
