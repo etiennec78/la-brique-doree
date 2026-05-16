545d75e7e31aa6fbe1f45380f2805d1e29faf195
Etienne C. <59794011+etiennec78@users.noreply.github.com>
2026-05-16 17:56:33 +0200

    Show active coupon

--- views/orders.php
+++ views/orders.php
@@ -72,7 +72,7 @@ include __DIR__ . '/../includes/header.php';
           <details class="coupon-details">
             <summary>Code promo ?</summary>
             <form class="coupon-form" action="/apply_coupon" method="POST">
-              <input type="text" name="coupon" placeholder="Code promo">
+              <input type="text" name="coupon" placeholder="Code promo" value="<?= $coupon['code'] ?>">
               <noscript>
                 <button id="submit_coupon" class="basic-btn" type="submit">Appliquer</button>
               </noscript>
