d05ca214c87cbf1b0077a8e3e30f25c7212a8e15
des7ruct <canaxel05@gmail.com>
2026-05-31 22:33:43 +0200

    Comments for homecontroller

--- src/controllers/HomeController.php
+++ src/controllers/HomeController.php
@@ -2,6 +2,22 @@
 
 class HomeController extends Controller {
     public function index() {
+        /*
+            
+         INPUT :
+                 
+            None
+          
+         OUTPUT :
+
+            None
+
+          
+         SUMMARY :
+            
+            Fetches total product amounts registered inside the user's shopping basket context and forwards tracking properties directly into the home landing view template.
+
+        */
         require_once __DIR__ . '/../models/Cart.php';
 
         $cart_count = Cart::getCartCount();
