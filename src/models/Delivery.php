4561e68a1ebb28efd2c36ce0a4b942230522ef90
des7ruct <canaxel05@gmail.com>
2026-05-31 21:03:23 +0200

    Comments for delivery

--- src/models/Delivery.php
+++ src/models/Delivery.php
@@ -3,6 +3,22 @@ require_once __DIR__ . '/../db_connect.php';
 
 class Delivery {
     public static function getDeliveries($uid) {
+	/*
+	 	
+	  INPUT :
+	         
+   	 	 (int) $uid : variable representing the delivery person user ID
+	  
+	  OUTPUT :
+
+ 		 (array) $deliveries : variable representing an array of active deliveries for the user
+
+	  
+	  SUMMARY :
+	 	
+		This function retrieves a list of all active shipping orders and customer details assigned to a specific delivery person.
+
+	*/
         global $pdo;
         $stmt = $pdo->prepare("
             SELECT o.id, u.first_name, u.last_name, u.street_nb, u.street_nb_suf, u.street, u.town, u.zip_code, u.intercom_code, u.latitude, u.longitude
@@ -16,6 +32,22 @@ class Delivery {
     }
 
     public static function getDeliveriesCoordinates($delivery_person_id) {
+	/*
+	 	
+	  INPUT :
+	         
+   	 	 (int) $delivery_person_id : variable representing the delivery person user ID
+	  
+	  OUTPUT :
+
+ 		 (array) $coordinates : variable representing an array containing the latitude and longitude coordinates
+
+	  
+	  SUMMARY :
+	 	
+		This function fetches the geographical coordinates of the customers for active shipping orders assigned to a specific delivery person.
+
+	*/
         global $pdo;
         $stmt = $pdo->prepare("
             SELECT u.latitude, u.longitude
@@ -29,6 +61,22 @@ class Delivery {
     }
 
     public static function buildMapURL($coordinates) {
+	/*
+	 	
+	  INPUT :
+	         
+   	 	 (array) $coordinates : variable representing an array of coordinates containing latitude and longitude
+	  
+	  OUTPUT :
+
+ 		 (str) $url : variable representing the generated OpenStreetMap embed URL
+
+	  
+	  SUMMARY :
+	 	
+		This function constructs a map URL from OpenStreetMap using the provided geographical coordinates and bounding box settings.
+
+	*/
         $baseUrl = "https://www.openstreetmap.org/export/embed.html";
         $params = [
             "bbox" => implode(',', [9.113277196884157, 55.72994659971866, 9.11605328321457, 55.73135269752343]),
